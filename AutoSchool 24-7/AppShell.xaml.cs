5a52b4a410891bceecd3ce1858f111f4a54227ae
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2025-02-24 07:41:07 +0500

    Add project

---
+++ AutoSchool 24-7/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoSchool_24_7
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AutoSchool 24-7/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AutoSchool_24_7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
