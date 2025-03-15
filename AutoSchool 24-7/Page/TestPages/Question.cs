f25fba8c1c380e77052d1c6aa430cd06846cda9f
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2025-03-15 21:04:09 +0500

    1

---
+++ AutoSchool 24-7/Page/TestPages/Question.cs
@@ -0,0 +1,18 @@
+﻿namespace AutoSchool_24_7.Page.TestPages
+{
+    public class Question
+    {
+        public string Text { get; }
+        public string Image { get; }
+        public string[] Options { get; }
+        public int CorrectAnswerIndex { get; }
+
+        public Question(string text, string image, string[] options, int correctAnswerIndex)
+        {
+            Text = text;
+            Image = image;
+            Options = options;
+            CorrectAnswerIndex = correctAnswerIndex;
+        }
+    }
+}
