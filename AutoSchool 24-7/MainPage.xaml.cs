667b4bd6951f2fb35ad3296e8075d7fe2e233611
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2025-04-09 22:43:12 +0500

    old ready

--- AutoSchool 24-7/MainPage.xaml.cs
+++ AutoSchool 24-7/MainPage.xaml.cs
@@ -2,25 +2,14 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        } 
-        private async void menuClicked(object sender, EventArgs e)
+        private async void OnGoToMenuClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new Menu());
         }
