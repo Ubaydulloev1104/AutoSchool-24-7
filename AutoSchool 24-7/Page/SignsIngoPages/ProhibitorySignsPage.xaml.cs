146ca3283714a662ca7e35852485091ff4560a3d
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2025-04-16 22:39:27 +0500

    add lounge

--- AutoSchool 24-7/Page/SignsIngoPages/ProhibitorySignsPage.xaml.cs
+++ AutoSchool 24-7/Page/SignsIngoPages/ProhibitorySignsPage.xaml.cs
@@ -5,5 +5,16 @@ public partial class ProhibitorySignsPage : ContentPage
 	public ProhibitorySignsPage()
 	{
 		InitializeComponent();
-	}
+        LoadDataAsync();
+
+    }
+    private async void LoadDataAsync()
+    {
+        // Имитация задержки для загрузки, можно заменить реальной логикой (например, загрузка из файла/сети)
+        await Task.Delay(1500);
+
+        // После "загрузки" скрываем панель загрузки и показываем контент
+        LoadingPanel.IsVisible = false;
+        MainContent.IsVisible = true;
+    }
 }
--- AutoSchool 24-7/Page/SignsIngoPages/ServiceSignsPage.xaml.cs
+++ AutoSchool 24-7/Page/SignsIngoPages/ServiceSignsPage.xaml.cs
@@ -5,5 +5,16 @@ public partial class ServiceSignsPage : ContentPage
 	public ServiceSignsPage()
 	{
 		InitializeComponent();
-	}
+        LoadDataAsync();
+
+    }
+    private async void LoadDataAsync()
+    {
+        // Имитация задержки для загрузки, можно заменить реальной логикой (например, загрузка из файла/сети)
+        await Task.Delay(1500);
+
+        // После "загрузки" скрываем панель загрузки и показываем контент
+        LoadingPanel.IsVisible = false;
+        MainContent.IsVisible = true;
+    }
 }
